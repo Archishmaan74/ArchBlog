a70d9166b5118dfc7babf897a31bf78bf30b8b67
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-06-25 13:47:47 +0530

    initialized spring boot for backend

---
+++ archblog-backend/src/main/java/com/archblog/archblog_backend/ArchblogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.archblog.archblog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchblogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchblogBackendApplication.class, args);
+	}
+
+}
---
+++ archblog-backend/src/test/java/com/archblog/archblog_backend/ArchblogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archblog.archblog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchblogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
