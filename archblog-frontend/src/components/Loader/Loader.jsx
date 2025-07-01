01b838c53816d90c523939efd323e30f1bbd2831
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-07-01 21:15:02 +0530

    removed inline css for Loader and implement styled component

--- archblog-frontend/src/components/Loader/Loader.jsx
+++ archblog-frontend/src/components/Loader/Loader.jsx
@@ -1,16 +1,31 @@
-import { CircularProgress, Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { CircularProgress } from "@mui/material";
+
+const StyledLoader = styled("div")(({ theme }) => ({
+  height: 600,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "inherit",
+  padding: theme.spacing(4),
+
+  [theme.breakpoints.down("md")]: {
+    height: 400,
+    padding: theme.spacing(2),
+  },
+
+  "& .loader-spinner": {
+    width: 40,
+    height: 40,
+    color: theme.palette.warning.main,
+    thickness: 4,
+  },
+}));
 
 const Loader = () => (
-  <Box
-    sx={{
-      minHeight: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <CircularProgress color="warning" />
-  </Box>
+  <StyledLoader>
+    <CircularProgress className="loader-spinner" />
+  </StyledLoader>
 );
 
 export default Loader;
