7fbc766ca0bd26ed7dd6cf8cabb72af4dd6f30f0
Archishmaan74 <archishmaan.upa4@gmail.com>
2025-07-04 13:11:19 +0530

    updated changes for fed deployment

--- archblog-frontend/src/app/services/authApi.js
+++ archblog-frontend/src/app/services/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_BASE_URL || "http://localhost:8080",
+    baseUrl: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
     prepareHeaders: (headers, { endpoint }) => {
       const publicEndpoints = [
         "postLoginUser",
